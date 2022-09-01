c2ac06071fadadce269c95673144687ae8c7319b
c0deMach1ne <contact@c0deMach1ne.com>
2022-09-01 10:53:08 +0300

    Basit tahmin oyunu eklendi.

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,30 @@
+﻿string secretWord = "c0deMach1ne";
+string guessWord = string.Empty;
+int guessCount = 0;
+int guessLimit = 3;
+bool outOfLimit = false;
+
+while (guessWord != secretWord && !outOfLimit)
+{
+    if (guessCount < guessLimit)
+    {
+        Console.Write("Enter guess: ");
+        guessWord = Console.ReadLine();
+        guessCount++;
+    }
+    else
+    {
+        outOfLimit = true;
+    }
+}
+
+if (outOfLimit)
+{
+    Console.WriteLine("You lose!");
+}
+else
+{
+    Console.WriteLine("You win!");
+}
+
+Console.ReadLine();
